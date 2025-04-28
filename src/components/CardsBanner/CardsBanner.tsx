64778db73735ad6f0d1db2eddea3e1ada4f3eccc
zahra noori sorkhani <zahranuori.1375@gmail.com>
2025-04-29 00:05:31 +0330

    create component cardbanner

---
+++ src/components/CardsBanner/CardsBanner.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+function CardsBanner() {
+  return (
+    <div>
+      <Card heading='Testimonial'>
+        <div>
+            <img src='' />
+            <h2></h2>
+            <h3></h3>
+            <img />
+            <p></p>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default CardsBanner
